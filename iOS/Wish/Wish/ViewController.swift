fe30235bb71a3572bd996b69f9d701c17f580d59
autel <autel@autel-zhong.local>
2017-04-22 17:16:02 +0800

    update

---
+++ iOS/Wish/Wish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wish
+//
+//  Created by autel on 17/4/22.
+//  Copyright © 2017年 zhongjiasheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
