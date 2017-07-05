e71ed5678cdf5bef7ac29626319fe6bec8f3be50
autel <autel@autel-zhong.local>
2017-07-05 20:09:20 +0800

    updata

---
+++ iOS/Transfer/Transfer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transfer
+//
+//  Created by autel on 17/7/5.
+//  Copyright © 2017年 zhongjiasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Transfer/Transfer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Transfer
+//
+//  Created by autel on 17/7/5.
+//  Copyright © 2017年 zhongjiasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
